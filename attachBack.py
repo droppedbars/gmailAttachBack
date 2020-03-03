9ca8f1f995490bc502bab3c29fde69ea5d0f61af
Patrick Mauro <patrick@mauro.ca>
2020-03-02 20:59:06 -0800

    started moving into functions. Added logging.

--- attachBack.py
+++ attachBack.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import base64
+import logging
 import os.path
 import pickle
 import pprint
@@ -10,77 +11,103 @@ from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
+logger = logging.getLogger("attachBack")
+
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+API_NAME = 'gmail'
+API_VER = 'v1'
 
-creds = None
-# The file token.pickle stores the user's access and refresh tokens, and is
-# created automatically when the authorization flow completes for the first
-# time.
-if os.path.exists('token.pickle'):
-    with open('token.pickle', 'rb') as token:
-        creds = pickle.load(token)
 
-# If there are no (valid) credentials available, let the user log in.
-if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(
-            'credentials-gmail.json', SCOPES)
-        creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open('token.pickle', 'wb') as token:
-        pickle.dump(creds, token)
+def authenticate(tokenFileName: str, credFileName: str):
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(tokenFileName):
+        with open(tokenFileName, 'rb') as token:
+            creds = pickle.load(token)
+            logger.info("Credentials read from %s", tokenFileName)
 
-service = build('gmail', 'v1', credentials=creds)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            logger.info("Credentials were expired, attempting to refresh.")
+            creds.refresh(Request())
+        else:
+            logger.info(
+                "Credentials could not be found, asking for authorization from the user.")
+            flow = InstalledAppFlow.from_client_secrets_file(
+                credFileName, SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(tokenFileName, 'wb') as token:
+            pickle.dump(creds, token)
+            logger.info("Credentials saved to %s for future use.",
+                        tokenFileName)
+    logging.info("Successfully authenticated to gmail.")
+    return creds
 
-messagelist = service.users().messages().list(userId='me').execute()
-for messageMeta in messagelist['messages']:
-    message = service.users().messages().get(
-        userId='me', id=messageMeta['id']).execute()
 
-    id = messageMeta['id']
-    subject = ''
-    contentType = ''
-    filename = ''
-    date = ''
+def main():
+    LOGFORMAT = "%(asctime)s %(levelname)s - %(name)s.%(funcName)s - %(message)s"
+    logging.basicConfig(level=logging.DEBUG, format=LOGFORMAT)
 
-    # pprint.pprint(message['payload']['headers'])
+    creds = authenticate('token.pickle', 'credentials-gmail.json')
 
-    for header in message['payload']['headers']:
-        if header['name'] == 'Subject':
-            subject = header['value']
-        if header['name'] == 'Date':
-            date = header['value']
-    if 'parts' in message['payload']:
-        for part in message['payload']['parts']:
-            for attachHeader in part['headers']:
-                if attachHeader['name'] == 'Content-Type':
-                    contentType = attachHeader['value']
-            if part['filename']:
-                filename = part['filename']
-                #print(date, '-', subject, '-', contentType, '-', filename)
-            if 'attachmentId' in part['body']:
-                print("foo: ", filename, contentType, subject)
-                # TODO: attachment is embedded, could use email subject and mimetime to set filename.ext
-                if not part['filename']:
-                    filename = 'fakename-'+str(time.time())
-                #    pprint.pprint(part)
-                # print(part['body']['attachmentId'])
-                attachmentId = part['body']['attachmentId']
+    service = build(API_NAME, API_VER, credentials=creds,
+                    cache_discovery=False)
 
-                attachment = service.users().messages().attachments().get(
-                    userId='me', messageId=id, id=attachmentId).execute()
+    messagelist = service.users().messages().list(userId='me').execute()
+    for messageMeta in messagelist['messages']:
+        message = service.users().messages().get(
+            userId='me', id=messageMeta['id']).execute()
 
-                file_data = base64.urlsafe_b64decode(
-                    attachment['data'].encode('UTF-8'))
+        id = messageMeta['id']
+        subject = ''
+        contentType = ''
+        filename = ''
+        date = ''
 
-                path = ''.join(['./fromgmail/', filename])
+        for header in message['payload']['headers']:
+            if header['name'] == 'Subject':
+                subject = header['value']
+            if header['name'] == 'Date':
+                date = header['value']
 
-                f = open(path, 'wb')
-                f.write(file_data)
-                f.close()
+        logger.debug("Checking message: %s from %s", subject, date)
+        if 'parts' in message['payload']:
+            for part in message['payload']['parts']:
+                for attachHeader in part['headers']:
+                    if attachHeader['name'] == 'Content-Type':
+                        contentType = attachHeader['value']
+                if part['filename']:
+                    filename = part['filename']
+                if 'attachmentId' in part['body']:
+                    # TODO: attachment is embedded, could use email subject and mimetime to set filename.ext
+                    if not part['filename']:
+                        filename = 'fakename-'+str(time.time())
+                        logger.info(
+                            "Attachment had no name. Named to %s from email subject %s", filename, subject)
+                    attachmentId = part['body']['attachmentId']
+
+                    logger.info(
+                        "Downloading attachment: %s from email subject %s", filename, subject)
+                    attachment = service.users().messages().attachments().get(
+                        userId='me', messageId=id, id=attachmentId).execute()
+
+                    file_data = base64.urlsafe_b64decode(
+                        attachment['data'].encode('UTF-8'))
+
+                    path = ''.join(['./fromgmail/', filename])
+
+                    f = open(path, 'wb')
+                    f.write(file_data)
+                    f.close()
+
+
+if __name__ == '__main__':
+    main()
 
 # TODO: iterate through response pages from gamil
 # TODO: create names based on subject and mimetype for unnamed attachments
@@ -89,3 +116,5 @@ for messageMeta in messagelist['messages']:
 # TODO: comment
 # TODO: document how to set up and create credentials
 # TODO: logger
+# TODO: store messageids that have been downloaded to prevent re-downloading on future runs.
+# TODO: configurable storage location
